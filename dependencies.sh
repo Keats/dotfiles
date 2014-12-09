2a4f0c02724a25450202dbc2988d901957a44fc7
Vincent Prouillet <vincent@wearewizards.io>
2014-12-09 22:49:02 +0000

    Start testing i3 and add redshift

--- dependencies.sh
+++ dependencies.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Terminal stuff
-sudo pacman -S rxvt-unicode zsh
+sudo pacman -S rxvt-unicode zsh xclip
 
 # File manager/usb automount stuff
 sudo pacman -S thunar thunar-volman udisks2 udiskie ntfs-3g
 
 # Misc
-sudo pacman -S dunst scrot feh evinces
+sudo pacman -S dunst scrot feh evince conky redshift
 
 # Dev
 sudo pacman -S vim python-pip python-virtualenv python-virtualenvwrapper
