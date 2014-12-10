6e272132601406d7042d314583cfd41318743c3e
Vincent Prouillet <vincent@wearewizards.io>
2014-12-10 21:02:30 +0000

    vista-fonts are in AUR, not in pacman

--- dependencies.sh
+++ dependencies.sh
@@ -10,7 +10,7 @@ sudo pacman -S i3 rxvt-unicode zsh xclip
 sudo pacman -S thunar thunar-volman udisks2 udiskie ntfs-3g
 
 # Fonts
-sudo pacman -S ttf-ubuntu-font-family ttf-vista-fonts ttf-inconsolata adobe-source-code-pro-fonts terminus-font adobe-source-sans-pro-fonts adobe-source-han-sans-jp-fonts ttf-droid
+sudo pacman -S ttf-ubuntu-font-family ttf-inconsolata adobe-source-code-pro-fonts terminus-font adobe-source-sans-pro-fonts adobe-source-han-sans-jp-fonts ttf-droid
 
 # Misc
 sudo pacman -S dunst scrot feh evince conky redshift firefox dmenu xf86-input-synaptics numix-themes
