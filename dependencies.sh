563e5c73c34ad0538071dee364415f13e5dba7d5
Vincent Prouillet <vincent@wearewizards.io>
2014-12-10 20:17:09 +0000

    Add i3 to dependencies

--- dependencies.sh
+++ dependencies.sh
@@ -3,14 +3,14 @@
 # X + graphic stuff
 sudo pacman -S xorg-server xorg-xinit xorg-server-utils mesa
 
-# Terminal stuff
-sudo pacman -S rxvt-unicode zsh xclip
+# Tiling/Terminal stuff
+sudo pacman -S i3 rxvt-unicode zsh xclip
 
 # File manager/usb automount stuff
 sudo pacman -S thunar thunar-volman udisks2 udiskie ntfs-3g
 
 # Misc
-sudo pacman -S dunst scrot feh evince conky redshift firefox
+sudo pacman -S dunst scrot feh evince conky redshift firefox dmenu
 
 # Wifi
 sudo pacman -S wpa_supplicant wireless_tools networkmanager network-manager-applet gnome-keyring gnome-icon-theme
