28808915c21b5dbac878c3625a8498cab306f2b8
Vincent Prouillet <vincent@wearewizards.io>
2014-12-12 23:14:56 +0000

    Add tranmission

--- dependencies.sh
+++ dependencies.sh
@@ -13,7 +13,7 @@ sudo pacman -S thunar thunar-volman udisks2 udiskie ntfs-3g
 sudo pacman -S ttf-liberation ttf-ubuntu-font-family ttf-inconsolata adobe-source-code-pro-fonts terminus-font adobe-source-sans-pro-fonts adobe-source-han-sans-jp-fonts ttf-droid
 
 # Misc
-sudo pacman -S arandr skype unclutter flashplugin dunst scrot feh evince conky redshift firefox dmenu xf86-input-synaptics numix-themes lxappearance
+sudo pacman -S tranmission-gtk arandr skype unclutter flashplugin dunst scrot feh evince conky redshift firefox dmenu xf86-input-synaptics numix-themes lxappearance
 
 # Wifi
 sudo pacman -S wpa_supplicant wireless_tools networkmanager network-manager-applet gnome-keyring gnome-icon-theme
