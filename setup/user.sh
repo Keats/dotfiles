ae6cd745cb006dbbac001854b392da8e68ccea47
Vincent Prouillet <vincent@wearewizards.io>
2015-03-11 20:36:35 +0000

    Play around with colors

--- setup/user.sh
+++ setup/user.sh
@@ -8,6 +8,7 @@ ln -sf /home/$USER/dotfiles/zshrc /home/$USER/.zshrc
 ln -sf /home/$USER/dotfiles/zprofile /home/$USER/.zprofile
 ln -sf /home/$USER/dotfiles/.gitconfig /home/$USER/.gitconfig
 ln -sf /home/$USER/dotfiles/Xresources /home/$USER/.Xresources
+ln -sf /home/$USER/dotfiles/.colors /home/$USER/.colors
 ln -sf /home/$USER/dotfiles/.config/bspwm /home/$USER/.config/bspwm
 ln -sf /home/$USER/dotfiles/.config/sxhkd /home/$USER/.config/sxhkd
 ln -sf /home/$USER/dotfiles/.config/lighthouse /home/$USER/.config/lighthouse
